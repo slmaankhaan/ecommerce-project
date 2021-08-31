70caf8e7b1740373ad93784bea7914b234ad3e67
slmaankhaan <slmaan.khaan@outlook.com>
2021-08-31 13:38:47 -0400

    Product-List
    Created Product list in Angular and send the data from spring boot ( back end).

--- 02-backend/spring-boot-ecommerce/src/main/java/com/khan/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/khan/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.khan.ecommerce.dao;
 
 import com.khan.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
