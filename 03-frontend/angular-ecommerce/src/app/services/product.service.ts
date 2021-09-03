303723134dc3de34100a63773e7471d3cb904780
slmaankhaan <slmaan.khaan@outlook.com>
2021-09-02 22:47:11 -0400

    Search for Product based on Category ID

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -13,8 +13,12 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    //  need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
@@ -23,5 +27,5 @@ export class ProductService {
 interface GetResponse {
   _embedded: {
     products: Product[];
-  }
+  };
 }
