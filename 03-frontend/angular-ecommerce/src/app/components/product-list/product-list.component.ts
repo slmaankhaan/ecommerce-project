49c774d57fe2460109a94fe8fa7668975393d8a6
slmaankhaan <slmaan.khaan@outlook.com>
2021-09-04 12:25:46 -0400

    Dynamic Search Component

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -13,25 +13,26 @@ export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(() =>{
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
-        }
-    );
-
+    });
   }
 
   listProducts() {
 
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
     if (hasCategoryId) {
-      //get the "id" param string. convert string to a number using the "+" symbol.
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')
-    } else {
-      //not category id availble ... default to category id 1
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
 
